4ffa6893a243854c1e1b14e3ee24bc3dfdafecbb
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-09-16 23:04:47 +0300

    made changes

--- postit/settings.py
+++ postit/settings.py
@@ -19,6 +19,8 @@ import cloudinary.api
 
 import dj_database_url
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get('DEBUG')
