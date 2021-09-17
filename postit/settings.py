f6470bb39985c2ba8ebe435a05a9299c259af531
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-09-17 14:26:18 +0300

    made changes

--- postit/settings.py
+++ postit/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = environ.get('DEBUG')
+DEBUG = environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
